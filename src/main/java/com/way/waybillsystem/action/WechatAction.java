6e915ae4ff8b3b0745350f40cffb61dc4daa2943
IOT <tech@atwulian.com>
2018-03-01 18:26:57 +0800

    add wechat code

--- src/main/java/com/way/waybillsystem/action/WechatAction.java
+++ src/main/java/com/way/waybillsystem/action/WechatAction.java
@@ -1,6 +1,11 @@
 package com.way.waybillsystem.action;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.github.pagehelper.PageInfo;
 import com.way.waybillsystem.entity.WechatToken;
 import com.way.waybillsystem.service.IWechatTokenService;
+import com.way.waybillsystem.util.WechatCheckUtil;
 import com.way.waybillsystem.vo.QueryByPageObject;
 
 
@@ -30,6 +36,33 @@ public class WechatAction  extends BaseAction {
 	@ResponseBody
 	public void deleteWechatTokenByPrimaryKey(Long id){
 		wechatTokenService.deleteWechatTokenByPrimaryKey(id);
+	}
+	
+	
+	@RequestMapping(value="/deleteWechatTokenByPrimaryKey",method=RequestMethod.POST)
+	@ResponseBody
+	public void wechatJoinup(HttpServletRequest request,HttpServletResponse response) throws IOException{
+	    // 将请求、响应的编码均设置为UTF-8（防止中文乱码）  
+        request.setCharacterEncoding("UTF-8");  //微信服务器POST消息时用的是UTF-8编码，在接收时也要用同样的编码，否则中文会乱码；
+        response.setCharacterEncoding("UTF-8"); //在响应消息（回复消息给用户）时，也将编码方式设置为UTF-8，原理同上
+//		signature	微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+//		timestamp	时间戳
+//		nonce	随机数
+//		echostr	随机字符串
+
+		String signature = request.getParameter("signature");
+		String timestamp = request.getParameter("timestamp");
+		String nonce = request.getParameter("nonce");
+		String echostr = request.getParameter("echostr");
+		String token = "WaybillSystem";
+		
+		PrintWriter out = response.getWriter();
+		if(WechatCheckUtil.checkSignature(token, signature, timestamp, nonce)){
+			out.print(echostr);
+			System.out.println("接入成功"+echostr);
+		}else{
+			System.out.println("接入失败");
+		}
 	}
 
 }
