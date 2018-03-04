f26f7e5c817b301ac54235ca6ad19bf8a2868304
WayLeung <610686949@qq.com>
2018-03-04 12:59:28 +0800

    定时器quartz有bug 用秒作定时时会出现bug 修复的话只能用大于秒的单位

--- src/main/java/com/way/waybillsystem/job/TimeJob.java
+++ src/main/java/com/way/waybillsystem/job/TimeJob.java
@@ -1,11 +1,13 @@
 package com.way.waybillsystem.job;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.way.waybillsystem.service.IWechatTokenService;
 
 public class TimeJob {
-	@Autowired
+	@Resource
 	IWechatTokenService wechatTokenService;
 	
 	public void getAndSaveAccessTokenJob(){
